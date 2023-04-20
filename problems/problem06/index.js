e68dbda68de8f673d79f11bdf11979de7086e93a
Jason Stieber <jrstieber@gmail.com>
2023-04-19 17:40:10 -0700

    boilerplate code for prob 6 added

---
+++ problems/problem06/index.js
@@ -0,0 +1,9 @@
+const data = require("./puzzleinput06");
+
+function findStartOfPacketLoc(packet) {
+    let loc = 0;
+
+    return loc;
+}
+
+module.exports = {findStartOfPacketLoc};
---
+++ problems/problem06/prob06.test.js
@@ -0,0 +1,25 @@
+//Advent of code 2022 day 6
+const prob6Funcs = require('./index.js');
+const myData = require("./puzzleinput06.js");
+
+describe('Day 6 Search for loction of first 4 non repeating characters', () => {
+  test('testing first test string should return 7', () => {
+    expect(prob6Funcs.findStartOfPacketLoc(myData.testString1)).toBe(7);
+  });
+  
+  test('testing second test string should return 5', () => {
+    expect(prob6Funcs.findStartOfPacketLoc(myData.testString2)).toBe(5);
+  });
+
+  test('testing third test string should return 6', () => {
+    expect(prob6Funcs.findStartOfPacketLoc(myData.testString3)).toBe(6);
+  });
+  test('testing fourth test string should return 10', () => {
+    expect(prob6Funcs.findStartOfPacketLoc(myData.testString4)).toBe(10);
+  });
+
+  test('testing fifth test string should return 11', () => {
+    expect(prob6Funcs.findStartOfPacketLoc(myData.testString4)).toBe(11);
+  });
+  
+});
