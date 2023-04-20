5ef45f1776e6562db9d0581a4fef27e8392f1ec5
Jason Stieber <jrstieber@gmail.com>
2023-04-19 18:43:39 -0700

    completed day 6 part a

--- problems/problem06/index.js
+++ problems/problem06/index.js
@@ -1,8 +1,22 @@
 const data = require("./puzzleinput06");
 
-function findStartOfPacketLoc(packet) {
-    let loc = 0;
+function nieveConditoinal(shortStr){
+    let a = shortStr[0];
+    let b = shortStr[1];
+    let c = shortStr[2];
+    let d = shortStr[3];
+    if(a === b || a === c || a === d || b === c || b === d || c === d) {
+        return false;
+    }
+    return true;
+}
 
+function findStartOfPacketLoc(packet) {
+    for(let i = 4, i < packet.length; i++){
+        if(nieveConditoinal(packet.slice(i-4,i))){
+            return i
+        }
+    }
     return loc;
 }
 
